a82dac6bef65caea9333fd3d6c7ded2c138e0c2d
Nagrajkamat07 <91514880+nagrajkamat@users.noreply.github.com>
2023-01-10 11:44:29 +0530

    Add files via upload

---
+++ Intoduction to Python/01_Find_average_Marks.py
@@ -0,0 +1,12 @@
+#Find average Marks
+
+#Write a program to input marks of three tests of a student (all integers). Then calculate and print the average of all test marks.
+# Read input as sepcified in the question
+# Print output as specified in the question
+a=int(input())
+b=int(input())
+c=int(input())
+
+average=(a+b+c)/3
+
+print(average)
