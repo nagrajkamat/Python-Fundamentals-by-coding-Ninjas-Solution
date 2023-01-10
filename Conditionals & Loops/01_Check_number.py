aef7da8ce4ce75ac4a2f7f5fd11f18409eabbb9f
Nagrajkamat07 <91514880+nagrajkamat@users.noreply.github.com>
2023-01-10 12:17:11 +0530

    Add files via upload

---
+++ Conditionals & Loops/01_Check_number.py
@@ -0,0 +1,22 @@
+#Given an integer n, find if n is positive, negative or 0. If n is positive, print "Positive", If n is negative, print "Negative" And if n is equal to 0, print "Zero".
+
+#approach 1 -
+n = int(input())
+
+if n>0 :
+    print("Positive")
+elif n<0:
+    print("Negative")
+else:
+    print("Zero")
+
+
+
+#Sample Input 1 -
+10
+#Sample Output 1 -
+Positive
+#Sample Input 2 -
+-10
+#Sample Output 2 -
+Negative
