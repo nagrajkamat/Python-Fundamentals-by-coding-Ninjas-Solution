7442fcee9f7b0979e5f3e8aebc2c33c040d84941
Nagrajkamat07 <91514880+nagrajkamat@users.noreply.github.com>
2023-01-10 12:07:48 +0530

    Add files via upload

---
+++ Intoduction to Python/03_Arithmetic_Progression.py
@@ -0,0 +1,8 @@
+#Arithmetic Progression
+#You are given first three entries of an arithmetic progression. You have to calculate the common difference and print it.
+
+a=int(input())
+b=int(input()) 
+c=int(input()) 
+common_difference = b-a
+print(common_difference) 
