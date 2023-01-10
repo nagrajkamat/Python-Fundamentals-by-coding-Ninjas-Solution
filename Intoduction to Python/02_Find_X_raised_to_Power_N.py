e102a858c2c5f9c682f22356bf5de5780a3d6dd3
Nagrajkamat07 <91514880+nagrajkamat@users.noreply.github.com>
2023-01-10 11:57:38 +0530

    Add files via upload

---
+++ Intoduction to Python/02_Find_X_raised_to_Power_N.py
@@ -0,0 +1,14 @@
+#Find X raised to power N
+#You are given two integers: X and N. You have to calculate X raised to power N and print it.
+
+x=int(input())
+n=int(input())
+res=pow(x,n)
+print(res)
+
+
+#Input-
+10
+4
+Output-
+10000
